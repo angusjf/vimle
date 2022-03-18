8c172e2c5cd9dc0495409961b56ab5320547dc6a
Angus Findlay <angusf@theodo.co.uk>
2022-03-18 23:38:22 +0000

    added share feature

--- src/Data.elm
+++ src/Data.elm
@@ -5,36 +5,234 @@ import Array
 
 data : Array.Array ( String, List String )
 data =
-    Array.fromList <|
-        [ ( "O.ZiH<ESC>x3D$D<ESC>", [ ".", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "x3D$D<ESC>1gqDI<ESC>", [ "  The quic", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "1gqDI<ESC>Zgw1V<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "Zgw1V<ESC>m1kJq<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "m1kJq<ESC>hVkcG<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "hVkcG<ESC>Degi2<ESC>", [ "", "  jumpe2d over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "Degi2<ESC>Rnnk2<ESC>", [ "", "  jumpednnk2er", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "Rnnk2<ESC>GChOe<ESC>", [ "nnk2quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   hOe" ] )
-        , ( "GChOe<ESC>aarCu<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   hOearCu" ] )
-        , ( "aarCu<ESC>EotIQ<ESC>", [ "TarCuhe quick brown fox", "tIQ", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "EotIQ<ESC>YLiAz<ESC>", [ "The quick brown fox", "tIQ", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   Azdog." ] )
-        , ( "YLiAz<ESC>GSV3i<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   V3i" ] )
-        , ( "GSV3i<ESC>qGlyX<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   V3i" ] )
-        , ( "qGlyX<ESC>fBa$.<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "fBa$.<ESC>klLzz<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "klLzz<ESC>kajP1<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "kajP1<ESC>aaH3R<ESC>", [ "TaH3Rhe quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "aaH3R<ESC>Vsmip<ESC>", [ "mip", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "Vsmip<ESC>cODQL<ESC>", [ "mip", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "cODQL<ESC>gLRUH<ESC>", [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "gLRUH<ESC>pmOOd<ESC>", [ "The quick brown fox", "d", "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "pmOOd<ESC>SKcWK<ESC>", [ "The quick brown fox", "KcWK", "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "SKcWK<ESC>OIz22<ESC>", [ "Iz22", "KcWK", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "OIz22<ESC>AjTCi<ESC>", [ "Iz22jTCi", "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "AjTCi<ESC>tkBP1<ESC>", [ "The quick brown foxjTCi", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "tkBP1<ESC>ppVWu<ESC>", [ "The quick brown fox", "The quick brown fox", "the quick brown fox", "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "ppVWu<ESC>iwPzb<ESC>", [ "The quick brown fox", "The quick brown fox", "wPzbthe quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "iwPzb<ESC>ss3Dn<ESC>", [ "wPzs3DnThe quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "ss3Dn<ESC>iOZsG<ESC>", [ "s3DOZsGnhe quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "iOZsG<ESC>rdePb<ESC>", [ "OZsdThTe quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        , ( "rdePb<ESC>CVdjr<ESC>", [ "Vdjr", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ] )
-        ]
+    Array.fromList <| List.map2 Tuple.pair commands results
+
+
+commands =
+    [ "O.ZiH<ESC>x3D$D<ESC>"
+    , "x3D$D<ESC>1gqDI<ESC>"
+    , "1gqDI<ESC>Zgw1V<ESC>"
+    , "Zgw1V<ESC>m1kJq<ESC>"
+    , "m1kJq<ESC>hVkcG<ESC>"
+    , "hVkcG<ESC>Degi2<ESC>"
+    , "Degi2<ESC>Rnnk2<ESC>"
+    , "Rnnk2<ESC>GChOe<ESC>"
+    , "GChOe<ESC>aarCu<ESC>"
+    , "aarCu<ESC>EotIQ<ESC>"
+    , "EotIQ<ESC>YLiAz<ESC>"
+    , "YLiAz<ESC>GSV3i<ESC>"
+    , "GSV3i<ESC>qGlyX<ESC>"
+    , "qGlyX<ESC>fBa$.<ESC>"
+    , "fBa$.<ESC>klLzz<ESC>"
+    , "klLzz<ESC>kajP1<ESC>"
+    , "kajP1<ESC>aaH3R<ESC>"
+    , "aaH3R<ESC>Vsmip<ESC>"
+    , "Vsmip<ESC>cODQL<ESC>"
+    , "cODQL<ESC>gLRUH<ESC>"
+    , "gLRUH<ESC>pmOOd<ESC>"
+    , "pmOOd<ESC>SKcWK<ESC>"
+    , "SKcWK<ESC>OIz22<ESC>"
+    , "OIz22<ESC>AjTCi<ESC>"
+    , "AjTCi<ESC>tkBP1<ESC>"
+    , "tkBP1<ESC>ppVWu<ESC>"
+    , "ppVWu<ESC>iwPzb<ESC>"
+    , "iwPzb<ESC>ss3Dn<ESC>"
+    , "ss3Dn<ESC>iOZsG<ESC>"
+    , "iOZsG<ESC>rdePb<ESC>"
+    , "rdePb<ESC>CVdjr<ESC>"
+    ]
+
+
+results =
+    [ [ "The quick brown fox"
+      , "jumped over"
+      , "."
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "th"
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "G"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "2"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jnnk2ed over"
+      , "the"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "thennk2y dog."
+      , " The quick brown fox jumped"
+      , " hOe"
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " hOearCu"
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the arCulazy dog."
+      , "tIQ"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , "tIQ"
+      , " The quick brown fox jumped"
+      , " Azover the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " V3i"
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " V3i"
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " TjP1he quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumpjP1aH3Red over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "mip"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "mip"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , "d"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , "KcWK"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "Iz22"
+      , "KcWK"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "Iz22jTCi"
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog.jTCi"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , "the lazy dog."
+      , "the lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "the lazy dog."
+      , "the lazy dog."
+      , "wPzbthe lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "thewPzs3Dn lazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "thes3DOZsGnlazy dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "theOZsd laz y dog."
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    , [ "The quick brown fox"
+      , "jumped over"
+      , "Vdjr"
+      , " The quick brown fox jumped"
+      , " over the lazy dog."
+      ]
+    ]
--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Array
 import Browser
@@ -12,54 +12,133 @@ import Element.Input
 type Msg
     = GuessChange String
     | OnSubmit
+    | OnShare String
+
+
+port copyToClipboard : String -> Cmd msg
 
 
 cursorPos =
-    ( 1, 0 )
+    ( 3, 4 )
 
 
 defaultGuess =
-    [ "The quick brown fox", "  jumped over", " the lazy dog.", "  The quick", "    brown fox jumped", "    over the lazy", "   dog." ]
+    [ "The quick brown fox", "jumped over", "the lazy dog.", " The quick brown fox jumped", " over the lazy dog." ]
 
 
-init =
-    { day = 1
-    , guess = String.join "\n" defaultGuess
-    , guesses = [ [] ]
-    }
+init : Int -> ( { day : Int, guess : String, guesses : List b, shared : Bool }, Cmd msg )
+init day =
+    ( { day = day
+      , guess = String.join "\n" defaultGuess
+      , guesses = []
+      , shared = False
+      }
+    , Cmd.none
+    )
 
 
-view { day, guess, guesses } =
+view { day, guess, guesses, shared } =
     let
         ( vimle, answer ) =
             Array.get day Data.data
                 |> Maybe.withDefault ( "UNDEFINED", [] )
-    in
-    Element.layout [ Element.Font.family [ Element.Font.monospace ] ] <|
-        Element.column []
-            [ Element.text <| "Vimle #" ++ String.fromInt day ++ " " ++ vimle
-            , Element.row []
-                [ viewQuick
-                , viewGuess { guess = guess }
+
+        game =
+            Element.column []
+                [ Element.row [ Element.spacing 20, Element.paddingXY 0 40 ]
+                    [ viewQuick
+                    , Element.column [ Element.width Element.fill ]
+                        [ viewInput { guess = guess }
+                        , Element.Input.button
+                            [ Element.alignRight
+                            ]
+                            { onPress = Just OnSubmit
+                            , label =
+                                Element.el
+                                    [ Element.Background.color black
+                                    , Element.Font.color white
+                                    ]
+                                <|
+                                    Element.text "Guess!"
+                            }
+                        ]
+                    ]
                 ]
-            , Element.Input.button [] { onPress = Just OnSubmit, label = Element.el [ Element.Background.color black, Element.Font.color white ] <| Element.text "Guess!" }
+    in
+    Element.layout
+        [ Element.Font.family [ Element.Font.typeface "consolas", Element.Font.monospace ]
+        , Element.Font.italic
+        , Element.paddingEach { top = 36, bottom = 0, left = 0, right = 0 }
+        ]
+    <|
+        Element.column [ Element.spacing 16, Element.centerX ]
+            [ Element.el [ Element.centerX ] <| Element.text <| "Vimle #" ++ String.fromInt day
+            , Element.el [ Element.centerX ] <| Element.text vimle
+            , case guesses of
+                [] ->
+                    game
+
+                latestGuess :: _ ->
+                    if latestGuess == answer || List.length guesses == 6 then
+                        Element.row
+                            [ Element.spacing 10 ]
+                            [ viewStatus { guesses = guesses, answer = answer }
+                            , viewShareButton { answer = answer, guesses = guesses, day = day } shared
+                            ]
+
+                    else
+                        game
             , viewGuesses { guesses = guesses, answer = answer }
             ]
 
 
 viewGuesses { guesses, answer } =
+    Element.column [ Element.spacing 10 ] <| List.map (viewGuess answer) guesses
+
+
+viewGuess answer guess =
+    guess
+        |> List.map2 Tuple.pair (List.range 1 (List.length defaultGuess))
+        |> List.map2 viewGuessLine (answer ++ List.repeat 10 "__________")
+        |> Element.column []
+
+
+viewStatus { guesses, answer } =
     case guesses of
+        final :: _ ->
+            Element.el [] <|
+                Element.text <|
+                    if final == answer then
+                        case List.length guesses of
+                            1 ->
+                                "Flawless!"
+
+                            2 ->
+                                "Incredible!"
+
+                            3 ->
+                                "Nice one!"
+
+                            4 ->
+                                "Well done!"
+
+                            5 ->
+                                "Well done!"
+
+                            6 ->
+                                "Phew!"
+
+                            _ ->
+                                "????"
+
+                    else
+                        "Bad luck!"
+
         [] ->
             Element.none
 
-        guess :: _ ->
-            guess
-                |> List.map2 Tuple.pair (List.range 1 (List.length defaultGuess))
-                |> List.map2 viewGuessLine (answer ++ List.repeat 10 "__________")
-                |> Element.column []
 
-
-viewGuess { guess } =
+viewInput { guess } =
     Element.Input.multiline []
         { onChange = GuessChange
         , text = guess
@@ -73,23 +152,27 @@ viewQuick =
     defaultGuess
         |> List.map2 Tuple.pair (List.range 1 (List.length defaultGuess))
         |> List.map viewQuickLine
-        |> Element.column []
+        |> Element.column [ Element.alignTop ]
 
 
 black =
-    Element.rgb 0.1 0.1 0.1
+    Element.rgb255 0 0 0
 
 
-red =
-    Element.rgb 0.9 0.1 0.1
+grey =
+    Element.rgb255 130 130 130
 
 
 green =
-    Element.rgb 0.1 0.9 0.1
+    Element.rgb255 100 170 100
+
+
+orange =
+    Element.rgb255 220 180 80
 
 
 white =
-    Element.rgb 0.9 1 1
+    Element.rgb255 255 255 255
 
 
 viewGuessLine answerLine ( no, line ) =
@@ -97,19 +180,24 @@ viewGuessLine answerLine ( no, line ) =
         ( cursorLine, cursorCol ) =
             cursorPos
     in
-    if line == answerLine then
-        Element.el [ Element.Background.color green, Element.Font.color white ] <|
-            Element.text <|
-                String.fromInt no
-                    ++ ". "
-                    ++ line
+    Element.el
+        [ Element.Background.color
+            (if line == answerLine then
+                green
 
-    else
-        Element.el [ Element.Background.color red, Element.Font.color white ] <|
-            Element.text <|
-                String.fromInt no
-                    ++ ". "
-                    ++ line
+             else if close line answerLine then
+                orange
+
+             else
+                grey
+            )
+        , Element.Font.color white
+        ]
+    <|
+        Element.text <|
+            String.fromInt no
+                ++ ". "
+                ++ line
 
 
 viewQuickLine ( no, line ) =
@@ -119,28 +207,103 @@ viewQuickLine ( no, line ) =
     in
     if cursorLine == no then
         Element.paragraph []
-            [ Element.text (String.fromInt no ++ ". ")
-            , Element.text (String.left cursorCol line)
-            , Element.el [ Element.Background.color black, Element.Font.color white ] <| Element.text (String.slice cursorCol (cursorCol + 1) line)
-            , Element.text (String.dropLeft (cursorCol + 1) line)
+            [ pre (String.fromInt no ++ ". " ++ String.left cursorCol line)
+            , Element.el [ Element.Background.color black, Element.Font.color white ] <| pre (String.slice cursorCol (cursorCol + 1) line)
+            , pre (String.dropLeft (cursorCol + 1) line)
             ]
 
     else
         Element.text <| String.fromInt no ++ ". " ++ line
 
 
+viewShareButton guesses copied =
+    Element.Input.button []
+        { onPress = Just <| OnShare (getSharable guesses)
+        , label =
+            Element.text <|
+                if copied then
+                    "copied!"
+
+                else
+                    "share"
+        }
+
+
+getSharable { guesses, answer, day } =
+    let
+        score =
+            case guesses of
+                guess :: _ ->
+                    if guess == answer then
+                        String.fromInt (List.length guesses)
+
+                    else
+                        "X"
+
+                _ ->
+                    "???"
+
+        title =
+            "Vimle " ++ String.fromInt day ++ " " ++ score ++ "/6\n"
+    in
+    title ++ (String.join "\n" <| List.map (asEmoji answer) (List.reverse guesses))
+
+
+asEmoji answer guess =
+    String.fromList <| customMap2 "!!!!!!!!!!!" isOk answer guess
+
+
+customMap2 : a -> (a -> a -> result) -> List a -> List a -> List result
+customMap2 u f listA listB =
+    case ( listA, listB ) of
+        ( [], [] ) ->
+            []
+
+        ( x :: xs, [] ) ->
+            f x u :: customMap2 u f xs []
+
+        ( [], y :: ys ) ->
+            f u y :: customMap2 u f [] ys
+
+        ( x :: xs, y :: ys ) ->
+            f x y :: customMap2 u f xs ys
+
+
+close real attempt =
+    String.contains attempt real || String.contains real attempt
+
+
+isOk attempt real =
+    if attempt == real then
+        '🟩'
+
+    else if close real attempt then
+        '🟨'
+
+    else
+        '⬜'
+
+
 update msg model =
     case msg of
         GuessChange guess ->
-            { model | guess = guess }
+            ( { model | guess = guess }, Cmd.none )
 
         OnSubmit ->
-            { model | guesses = String.split "\n" model.guess :: model.guesses }
+            ( { model | guesses = String.split "\n" model.guess :: model.guesses }, Cmd.none )
+
+        OnShare txt ->
+            ( { model | shared = True }, copyToClipboard txt )
 
 
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
+
+
+pre text =
+    Element.text text
